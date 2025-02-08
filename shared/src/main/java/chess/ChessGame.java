6914f5b480d6d631eee681425a3a5444dd920e39
Jacob Larson <jl2379@byu.edu>
2025-02-07 21:45:28 -0700

    Finished all team turn logic in ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -20,7 +20,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return this.teamTurn;
     }
 
     /**
@@ -29,9 +29,15 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.teamTurn = team;
     }
-
+    private void switchTeams(TeamColor currentTurn) {
+        if (currentTurn == TeamColor.WHITE) {
+            teamTurn = TeamColor.BLACK;
+        } else {
+            teamTurn = TeamColor.WHITE;
+        }
+    }
     /**
      * Enum identifying the 2 possible teams in a chess game
      */
